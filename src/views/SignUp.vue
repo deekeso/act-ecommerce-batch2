eeee2fcebfb180d397c8a53bb547f26555001daa
FCL00 <flagahit00@gmail.com>
2025-07-28 15:17:44 +0800

    feature: display sign up page

---
+++ src/views/SignUp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <register-form title="Sign up" btn-label="Sign up" mode="signup" @on-submit="handleSubmit" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import { AuthForm as RegisterForm } from '@/components'
+import { useAuth } from '@/stores/auth'
+import { useAuthGuard } from '@/composables/useAuthGuard'
+import type { User } from '@/types'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const authStore = useAuth()
+const { requireGuest } = useAuthGuard()
+
+onMounted(() => {
+  requireGuest()
+})
+
+function handleSubmit(formData: User) {
+  authStore.handleSignUp(formData)
+
+  if (authStore.isAuthenticated) {
+    router.push('/profile')
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  height: 70vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
