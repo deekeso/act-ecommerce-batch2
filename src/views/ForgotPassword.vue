1ba0362d7477a96362bb63adaaa66c6c8294c37e
FCL00 <flagahit00@gmail.com>
2025-07-28 15:02:47 +0800

    feature: display forgot password page

---
+++ src/views/ForgotPassword.vue
@@ -0,0 +1,26 @@
+<template>
+    <main class="container">
+        <forgot-password @on-submit="handlePasswordReset"/>
+    </main>
+</template>
+
+<script setup lang="ts">
+import { ForgotPasswordForm  as ForgotPassword } from '@/components';
+import { useAuth } from '@/stores/auth'
+import { ElMessage } from 'element-plus'
+
+const auth = useAuth()
+
+function handlePasswordReset(email: string) {
+  auth.forgotPassword({ email })
+}
+</script>
+
+<style scoped>
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 70vh;
+}
+</style>
