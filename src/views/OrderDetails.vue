7e18047b19910a95cabd517314f0b3467f152947
FCL00 <flagahit00@gmail.com>
2025-07-28 15:07:58 +0800

    feature: display the order history and details

---
+++ src/views/OrderDetails.vue
@@ -0,0 +1,200 @@
+<template>
+  <profile-layout>
+    <div class="order-page-wrapper">
+      <h2 class="line-clamp">Order Id #{{ orderDetails?.id }}</h2>
+      <el-collapse v-model="activeNames">
+        <el-collapse-item title="Order Summary" name="1">
+          <div style="padding: 20px">
+            <h3>Delivery Information</h3>
+            <p>
+              <span>Fullname: </span>
+              {{ orderDetails?.address?.firstname }}
+              {{ orderDetails?.address?.lastname }}
+            </p>
+            <p>Email: {{ orderDetails?.address?.email }}</p>
+            <p>Contact No. {{ orderDetails?.address?.contact }}</p>
+            <p>Address: {{ orderDetails?.address?.address }}</p>
+            <h3>Transaction Fee</h3>
+            <p class="flex-text">
+              Shipping Fee
+              <span>{{ formatPrice(orderDetails?.shippingFee ?? 0) }}</span>
+            </p>
+            <!-- <p class="flex-text">
+            VAT
+            <span>{{ formatPrice(0) }}</span>
+          </p> -->
+           <p class="flex-text">
+              Sub Total
+              <span>{{ formatPrice((orderDetails?.totalPrice ?? 0)) }}</span>
+            </p>
+            <p class="flex-text">
+              Total Amount
+              <span>{{ formatPrice((orderDetails?.totalPrice ?? 0) + (orderDetails?.shippingFee?? 0)) }}</span>
+            </p>
+            <h3>Delivery Options</h3>
+            <p>
+              {{ orderDetails?.shippingFee === 100 ? "Standard " : "Express " }}
+              Delivery
+            </p>
+            <h3>Payment Method</h3>
+            <p>Cash on Delivery</p>
+          </div>
+        </el-collapse-item>
+        <el-collapse-item :title="`${orderTitle} Ordered`" name="2">
+          <div
+            class="order-card-wrapper"
+            v-for="order in orderDetails?.items"
+            :key="order.id"
+          >
+            <el-image :src="order.image" fit="cover" :lazy="true" />
+            <div class="order-card-body">
+              <p>{{ order.label }}</p>
+              <p>{{ order.quantity }} X {{ formatPrice(order.price) }}</p>
+              <p>{{ formatPrice(order.quantity * order.price) }}</p>
+            </div>
+          </div>
+        </el-collapse-item>
+
+        <el-collapse-item title="Tracking Information" name="3">
+          <div style="padding: 0 20px">
+            <time-line
+              :order-id="orderDetails?.id"
+              :status="orderDetails?.status"
+            />
+          </div>
+        </el-collapse-item>
+      </el-collapse>
+    </div>
+  </profile-layout>
+</template>
+
+<script setup lang="ts">
+import { onMounted, watch, ref, computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { useOrder } from "@/stores/orders";
+import type { Order } from "@/types";
+import { ElMessage } from "element-plus";
+import { useUtils } from "@/composables/useUtils";
+import { TimeLine } from "@/components";
+import ProfileLayout from "@/layouts/ProfileLayout.vue";
+
+const activeNames = ref(["1"]);
+const orderStore = useOrder();
+const route = useRoute();
+const router = useRouter();
+const orderDetails = ref<Partial<Order>>();
+const orderTitle = computed(() =>
+  (orderDetails.value?.items?.length ?? 0) > 1 ? "Items" : "Item"
+);
+const { formatPrice } = useUtils();
+
+function loadOrderDetails(id: string) {
+  orderDetails.value = orderStore.getOrderById(id);
+}
+
+onMounted(() => {
+  const orderId = route.params.id as string;
+  const isOwner = orderStore.doesCurrentUserOwnOrder(orderId);
+
+  if (isOwner) {
+    loadOrderDetails(orderId);
+  } else {
+    router.push("/");
+    ElMessage.error("Unauthorized Action!");
+  }
+});
+
+watch(
+  () => route.params.id,
+  (newId) => {
+    const isOwner = orderStore.doesCurrentUserOwnOrder(newId as string);
+    if (isOwner) {
+      loadOrderDetails(newId as string);
+    } else {
+      ElMessage.error("Unauthorized Action!");
+    }
+  }
+);
+</script>
+
+<style scoped>
+.order-page-wrapper {
+  margin: 0 auto;
+}
+
+:deep(.el-collapse-item__title) {
+  font-weight: bold;
+  font-size: 18px;
+  padding: 20px;
+}
+
+p {
+  margin: 0.3rem 0;
+}
+
+.el-collapse {
+  border: 1px solid #e0e0e0;
+  border-radius: 6px;
+}
+.order-card-wrapper {
+  display: flex;
+  gap: 12px;
+  margin: 12px;
+}
+
+.el-image {
+  width: 100px !important;
+  height: 100px !important;
+  border-radius: 8px;
+  object-fit: cover;
+  border: 1px solid #e0e0e0;
+}
+
+.el-card {
+  margin-top: 1.5rem;
+  border: 1px solid #e0e0e0;
+  border-radius: 6px;
+}
+
+.flex-text {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+  font-weight: 500;
+}
+
+/* Tablet (<= 1024px) */
+@media (max-width: 1024px) {
+  .grid-wrapper {
+    grid-template-columns: 100px 1fr;
+    grid-template-rows: auto auto;
+    row-gap: 0.5rem;
+  }
+
+  .grid-wrapper p:nth-child(2),
+  .grid-wrapper p:nth-child(3),
+  .grid-wrapper p:nth-child(4) {
+    grid-column: 2 / span 1;
+  }
+}
+
+/* Mobile (<= 600px) */
+@media (max-width: 600px) {
+  .grid-wrapper {
+    grid-template-columns: 1fr;
+    grid-template-rows: auto;
+  }
+
+  .el-image {
+    width: 100%;
+    height: auto;
+    max-height: 200px;
+  }
+
+  .flex-text {
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 0.3rem;
+  }
+}
+</style>
---
+++ src/views/OrderView.vue
@@ -0,0 +1,80 @@
+<template>
+  <profile-layout>
+    <el-card class="history" shadow="never">
+      <h1 class="sticky-heading">Purchase History</h1>
+      <el-collapse
+        v-if="orderStore.getOrdersForCurrentUser.length > 0"
+        v-model="activeName"
+        v-for="(order, index) in orderStore.getOrdersForCurrentUser"
+        :key="order.id"
+        expand-icon-position="left"
+      >
+        <el-collapse-item :name="index + 1">
+          <template #title>
+            <div class="accordion-title">
+              <h3 class="line-clamp">Order Id #{{ order.id }}</h3>
+              <el-tag v-if="order.status === 'pending'">{{
+                order.status
+              }}</el-tag>
+              <el-tag v-if="order.status === 'delivered'" type="success">
+                {{ order.status }}
+              </el-tag>
+            </div>
+          </template>
+          <el-card shadow="never">
+            <p>Order Date: {{ formattedDate(order.createdAt) }}</p>
+            <p>Order Status: {{ order.status }}</p>
+            <p>Total Number of items: {{ order.items.length }}</p>
+            <p>Total Amount {{ formatPrice(order.totalPrice) }}</p>
+
+            <el-button color="black" @click="viewOrderDetails(order.id)">
+              View Details
+            </el-button>
+          </el-card>
+        </el-collapse-item>
+      </el-collapse>
+      <el-empty v-if="orderStore.getOrdersForCurrentUser.length < 0" description="Purchase history is empty">
+        <router-link to="/products">Shop Now!</router-link>
+      </el-empty>
+    </el-card>
+  </profile-layout>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { useOrder } from "@/stores/orders";
+import { useRouter } from "vue-router";
+import { useUtils } from "@/composables/useUtils";
+import ProfileLayout from "@/layouts/ProfileLayout.vue";
+
+const orderStore = useOrder();
+const activeName = ref(1);
+const router = useRouter();
+const { formatPrice } = useUtils();
+const formattedDate = computed(() => (date: string) => new Date(date).toLocaleDateString("en-US") )
+
+function viewOrderDetails(id: string) {
+  router.push(`/order/${id}`);
+}
+</script>
+
+<style scoped>
+.history{
+  height: 500px;
+  overflow-y: scroll;
+}
+.sticky-heading{
+  position: sticky;
+  background-color: white;
+  top: 0;
+}
+
+.accordion-title {
+  display: flex;
+  justify-content: space-between;
+}
+:deep(.el-card__header) {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
