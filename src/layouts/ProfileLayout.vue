75076942c89f7d4c021b01ee1e74bcee08d2c068
FCL00 <flagahit00@gmail.com>
2025-07-28 13:55:19 +0800

    feature: add profile layout to use as the layout for the profile pages

---
+++ src/layouts/ProfileLayout.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="container">
+    <div class="account-settings">
+      <aside class="sidebar">
+        <h1>Account Settings</h1>
+        <router-link to="/profile" exact-active-class="active">My Profile</router-link>
+        <router-link to="/account-security" exact-active-class="active">Account Security</router-link>
+        <router-link to="/order" exact-active-class="active">Purchase History</router-link>
+      </aside>
+      
+      <main class="content">
+        <slot name="default" />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<style scoped>
+.container {
+  max-width: 1400px;
+  margin: 0 auto;
+  padding: 40px 24px;
+  box-sizing: border-box;
+}
+
+.account-settings {
+  display: grid;
+  grid-template-columns: 260px 1fr;
+  gap: 24px;
+}
+
+.sidebar {
+  background: #fff;
+  border-radius: 8px;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  /* box-shadow: 0 1px 3px rgba(0, 0, 0, 0.05); */
+}
+
+.sidebar a {
+  color: #333;
+  text-decoration: none;
+  font-weight: 500;
+  padding: 16px;
+  border-radius: 6px;
+  transition: background 0.2s ease;
+}
+
+.sidebar a.active,
+.sidebar a:hover {
+  background-color: #eef2f8;
+  color: #1f2937;
+}
+
+.content {
+  background: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  /* box-shadow: 0 1px 3px rgba(0, 0, 0, 0.05); */
+
+}
+
+@media (max-width: 768px) {
+  .container{
+    height: 100%;
+  }
+  .account-settings {
+    grid-template-columns: 1fr;
+  }
+
+  .sidebar {
+    flex-direction: row;
+    align-items: center;
+    overflow-x: auto;
+  }
+
+  .sidebar a {
+    white-space: nowrap;
+  }
+}
+</style>
