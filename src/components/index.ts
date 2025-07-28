fa285945b12a0f6d18dcabb893c6f6d2ae018880
FCL00 <flagahit00@gmail.com>
2025-07-28 13:03:44 +0800

    chore: add the export path for all the components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,37 @@
+import AuthForm from './auth/AuthForm.vue'
 import BaseHeader from './ui/BaseHeader.vue'
 import ActionLinks from './ui/ActionLinks.vue'
-import ProductCard from './ui/ProductCard.vue'
-import ProductList from './ProductList.vue'
+import ProductCard from './products/ProductCard.vue'
+import ProductList from './products/ProductList.vue'
 import BaseFooter from './ui/BaseFooter.vue'
 import BaseBanner from './BaseBanner.vue'
 import BasePill from './ui/BasePill.vue'
-import PopularProducts from './PopularProducts.vue'
+import PopularProducts from './products/PopularProducts.vue'
+import SearchBar from './ui/SearchBar.vue'
+import CartCard from './cart/CartCard.vue'
+import AddressForm from './address/AddressForm.vue'
+import AddressCard from './address/AddressCard.vue'
+import AddressContainer from './address/AddressContainer.vue'
+import ProfileForm from './profile/ProfileForm.vue'
+import SummaryCard from './order/SummaryCard.vue'
+import ReviewCard from './order/ReviewCard.vue'
+import TimeLine from './ui/TimeLine.vue'
+import OrderCard from './order/OrderCard.vue'
+import ForgotPasswordForm from './auth/ForgotPasswordForm.vue'
+
 export {
+  ForgotPasswordForm,
+  OrderCard,
+  TimeLine,
+  ReviewCard,
+  SummaryCard,
+  ProfileForm,
+  AuthForm,
+  AddressContainer,
+  AddressCard,
+  SearchBar,
+  CartCard,
+  AddressForm,
   ProductCard,
   BaseHeader,
   ActionLinks,
