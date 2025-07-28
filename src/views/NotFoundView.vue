302804d81e5868c82edb4099ff294bb3c0d09b5c
FCL00 <flagahit00@gmail.com>
2025-07-28 15:03:43 +0800

    feature: display 404 not found page on unknown path

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1>NOT FOUND</h1>
+    <router-link to="/">GO BACK HOME &rarr;</router-link>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 80vh;
+}
+
+a {
+  color: black;
+}
+
+h1 {
+  font-size: 7rem;
+}
+</style>
