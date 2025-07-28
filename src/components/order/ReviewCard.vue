5e388d4a25cabf0be0b3c76dabb906f7312ac58a
FCL00 <flagahit00@gmail.com>
2025-07-28 13:25:05 +0800

    feature: display the review all the items that is currently on order page

---
+++ src/components/order/ReviewCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="order-list">
+    <div class="order-card" v-for="item in currentItem" :key="item?.id">
+      <el-image 
+        :src="item?.image" 
+        fit="cover" 
+        :lazy="true" 
+        style="width: 100px; height: 100px; border-radius: 12px" 
+      />
+      <div class="order-card-body">
+        <p>{{ item?.label }}</p>
+        <p>
+          Price: <span class="price-tag">{{ formatPrice(item?.price ?? 0) }}</span>
+        </p>
+        <p>Quantity: {{ item?.quantity }}x</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCart } from "@/stores/carts";
+import { useUtils } from "@/composables/useUtils";
+import { computed } from "vue";
+
+const cartStore = useCart();
+const { formatPrice } = useUtils();
+
+const buyNowItem = computed(() => cartStore.getBuyNowItems);
+const isBuyNowMode = computed(() => buyNowItem.value !== null && Object.keys(buyNowItem.value).length > 0);
+
+// Decide what to render based on Buy Now mode
+const currentItem = computed(() =>
+  isBuyNowMode.value ? [buyNowItem.value] : cartStore.getAllSelectedCartItems
+);
+</script>
+
+<style scoped>
+.order-card {
+  display: flex;
+  margin-bottom: 20px;
+}
+
+.order-card-body {
+  padding: 20px;
+}
+
+.order-list {
+  height: 100%;
+  min-height: 620px;
+  position: relative;
+  overflow-y: scroll;
+}
+</style>
