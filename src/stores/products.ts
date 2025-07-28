080017c587dc3cd7cf81850f4b5756c64f202467
FCL00 <flagahit00@gmail.com>
2025-07-28 14:10:18 +0800

    feature: add product store that handles ff:
    - get the product by id
    - get the search by user input
    - display the list of products

--- src/stores/products.ts
+++ src/stores/products.ts
@@ -5,7 +5,12 @@ export const useProducts = defineStore('products', {
     products: JSON.parse(localStorage.getItem('products') || '[]') as Products[],
   }),
 
-  getters: {},
+  getters: {
+   getSearchItem: (state) => (text: string) =>
+    state.products.filter(item =>
+      item.label?.toLowerCase().includes(text.toLowerCase())
+    )
+  },
 
   actions: {
     getProductById(id: string) {
@@ -14,5 +19,6 @@ export const useProducts = defineStore('products', {
     saveOnLocalStorage() {
       localStorage.setItem('products', JSON.stringify(this.products))
     },
+
   },
 })
