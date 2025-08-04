13b1a88016b1b28fedbe9bf842d60d8004f1c1a4
FCL00 <flagahit00@gmail.com>
2025-08-04 16:53:00 +0800

    feature: add a dropdown links for easy access on mobile screen

--- src/components/ui/BaseHeader.vue
+++ src/components/ui/BaseHeader.vue
@@ -9,13 +9,27 @@
         <search-bar />
       </div>
       <div class="flex-links">
-        <router-link to="/profile" class="link" v-if="authStore.token">
-          <el-icon :size="20"><User /></el-icon>
-          <!-- <p>Profile</p> -->
-        </router-link>
+        <el-dropdown v-if="authStore.token" trigger="click">
+          <span class="link" role="button">
+            <el-icon :size="20"><User /></el-icon>
+            My Account
+          </span>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item @click="router.push('/profile')">Profile</el-dropdown-item>
+              <el-dropdown-item @click="router.push('/account-security')">Manage Account</el-dropdown-item>
+              <el-dropdown-item @click="router.push('/order')">Purchase History</el-dropdown-item>
+              <el-dropdown-item @click="handleLogout">Logout</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+
         <router-link to="/cart" class="link" role="button">
           <el-icon :size="20"><ShoppingCart /></el-icon>
-          <!-- <p>Cart</p> -->
+          <span>Cart</span>
+          <span v-if="cartStore.getCartItems.length > 0" class="">
+            {{ authStore.token != null ? `(${cartStore.getCartItems.length})` : '' }}
+          </span>
         </router-link>
       </div>
     </nav>
@@ -25,12 +39,28 @@
 <script lang="ts" setup>
 import TheActionLinks from './ActionLinks.vue'
 import { SearchBar } from '@/components'
-import { ElIcon } from 'element-plus'
+import { ElIcon, ElMessageBox } from 'element-plus'
 import { ShoppingCart, User } from '@element-plus/icons-vue'
 import { RouterLink } from 'vue-router'
 import { useAuth } from '@/stores/auth'
+import { useRouter } from 'vue-router'
+import { useCart } from '@/stores/carts'
 
+const cartStore = useCart()
+const router = useRouter()
 const authStore = useAuth()
+
+function handleLogout() {
+  ElMessageBox.confirm('Do you really want to logout?', 'Warning', { type: 'warning' })
+    .then(() => {
+      authStore.handleLogout()
+      cartStore.clearBuyNow()
+      router.push('/')
+    })
+    .catch(() => {
+      return
+    })
+}
 </script>
 
 <style scoped>
@@ -38,6 +68,17 @@ const authStore = useAuth()
   display: flex;
   align-content: center;
   gap: 12px;
+}
+
+.cart-item-counter {
+  /* position: absolute;
+  top: 2px;
+  right: 4px; */
+  font-size: x-small;
+  background-color: #000;
+  color: white;
+  padding: 2px 7px;
+  border-radius: 50%;
 }
 
 .flex-links a {
@@ -68,18 +109,26 @@ const authStore = useAuth()
 
 /* links */
 .link {
+  color: #000;
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 4px;
   cursor: pointer;
+  position: relative;
 }
 
 .search-bar-form {
   width: 500px;
 }
 
-@media (max-width: 750px) {
+@media (max-width: 853px) {
+  .search-bar-form {
+    width: 400px;
+  }
+}
+
+@media (max-width: 768px) {
   .search-bar-form {
     display: none;
   }
