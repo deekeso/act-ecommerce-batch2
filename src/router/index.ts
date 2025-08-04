312bb9ebe7c6f37a5c7197abaf20990e50bf8611
FCL00 <flagahit00@gmail.com>
2025-08-04 16:54:56 +0800

    refractor: from routes, to routes: routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import { ElMessage } from 'element-plus'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  routes: routes,
   scrollBehavior() {
     return { top: 0, left: 0 }
   },
