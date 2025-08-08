3a10ba5b834d7aebdbc3aab2f102db36dced8327
FCL00 <flagahit00@gmail.com>
2025-08-08 16:44:51 +0800

    bug fix: increase the password length needed

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -51,7 +51,7 @@ export const useAuth = defineStore('auth', {
         cancelButtonText: 'Cancel',
         inputType: 'password',
         inputPattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/,
-        inputErrorMessage: 'Password must be at least 6 characters long',
+        inputErrorMessage: 'Password must be at least 8 characters long',
       })
         .then(({ value }) => {
           this.users[existingUserIndex].password = value
